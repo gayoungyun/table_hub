27d6d352c3d0dce83137ab9de9f09f5a73276351
Hi <cjdvhen@naver.com>
2024-06-05 09:32:33 +0900

    2일차

--- src/main/java/com/hub/root/board/BoardController.java
+++ src/main/java/com/hub/root/board/BoardController.java
@@ -1,5 +1,17 @@
 package com.hub.root.board;
 
-public class board {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+@RequestMapping("board")
+public class BoardController {
+	@Autowired BoardService bs;
+	
+	@GetMapping("list")
+	public String board() {
+		return "board/list";
+	}
 }
